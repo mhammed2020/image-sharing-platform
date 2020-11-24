fb69c0b18baa44291286e32f8b4a2593d5c9a7ba
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-24 16:27:37 +0100

    Add template name to CBV reset confirm

--- account/urls.py
+++ account/urls.py
@@ -23,12 +23,15 @@ name='password_change_done'),
 path('password_reset/',
 auth_views.PasswordResetView.as_view(),
 name='password_reset'),
+
 path('password_reset/done/',
 auth_views.PasswordResetDoneView.as_view(),
 name='password_reset_done'),
+
 path('reset/<uidb64>/<token>/',
-auth_views.PasswordResetConfirmView.as_view(),
+auth_views.PasswordResetConfirmView.as_view( template_name='registration/password_reset_ confirm.html'),
 name='password_reset_confirm'),
+
 path('reset/done/',
 auth_views.PasswordResetCompleteView.as_view(),
 name='password_reset_complete'),
