ff12253279875ccdd95ba13b5becec96be81504f
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-24 19:41:11 +0100

    update database using  django external form

--- account/urls.py
+++ account/urls.py
@@ -8,6 +8,7 @@ path('login/', auth_views.LoginView.as_view(), name='login'),
 path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
 path('', views.dashboard, name='dashboard'),
+
 path('register/', views.register, name='register'),
 
 
