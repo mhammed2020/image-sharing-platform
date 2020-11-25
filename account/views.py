c13ef0883bfde24050356ce9d74336cb3a5a698e
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-25 13:48:35 +0100

    Add social auth with django with social-auth-app-django

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm,UserRegistrationForm,UserEditForm, ProfileEditForm
 from .models import Profile
-
+from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required
 @login_required
@@ -43,6 +43,11 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')       
+
+        else:
+            messages.error(request, 'Error updating your profile')
+
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
---
+++ help.md
@@ -0,0 +1,13 @@
+# Heading1 Mastering python 3.x
+
+
+
+## page 129  instagram project
+
+
+* item 1
+* Item 2
+- Item 3
+- Item 4
++ item 5
++ item 5
